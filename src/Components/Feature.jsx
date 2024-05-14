00cd4faf9ff2e504cecfb731e57001f433489172
hdkid7 <hameddiakite34@yahoo.com>
2024-05-13 20:09:19 -0400

    added default descriptions

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -2,9 +2,9 @@ export default function Feature({keywords, name, description}) {
     const keywordStr = keywords.join(", ")
 
     return(
-        <div className="m-10">
+        <div className="m-5">
             <h3><span className="font-bold">Feature Name:</span> {name}</h3>
-            <p><span className="font-bold">Feature Description:</span> {description}</p>
+            <p><span className="font-bold">Feature Description:</span> {description ?? <i>No Description Provided...</i>}</p>
             <p className="text-sm"><span className="font-bold">Keywords:</span> {keywordStr}</p>
         </div>
     )
--- src/Components/FeatureCategory.jsx
+++ src/Components/FeatureCategory.jsx
@@ -4,9 +4,9 @@ export default function FeatureCategory({name, featuresArr, description}) {
     if(! featuresArr.length) return '';
 
     return(
-        <div className="text-left text-white m-5">
+        <div className="text-left text-white ml-20 mr-20 mt-5 mb-5">
             <h1 className="text-2xl text-left font-bold">{name}</h1>
-            <h2 className="text-l">{description}</h2>
+            <h2 className="text-l">{description ?? <i>No Description Provided...</i>}</h2>
             <ul>
                 {featuresArr.map(feature => <Feature description={feature.optionalDescription} keywords={feature.epKeywords} name={feature.displayName}/>)}
             </ul>
